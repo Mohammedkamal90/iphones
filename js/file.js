464e36ec636fa5b2dee0832a35b2730a6cc88ddc
M0.Kamal <113346017+Mohammedkamal90@users.noreply.github.com>
2022-12-12 08:10:25 +0200

    Add files via upload
    sample Iphone project

---
+++ js/file.js
@@ -0,0 +1,14 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+let btn = document.querySelector(".btn");
+function phones(phone) {
+  img.src = phone;
+}
+
+function colors(color) {
+  container.style.background = color;
+}
+
+function bton(clr) {
+  btn.style.background = color;
+}
